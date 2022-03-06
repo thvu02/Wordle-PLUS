3c56828a02c1219b18ef12e16fd30541f2e16e7c
Adrian Lam <51703086+AdrianLamLH@users.noreply.github.com>
2022-03-06 14:53:42 -0800

    Added accessiblity for Backspace and Enter keyboard entries

--- frontend/src/components/Gamepage.js
+++ frontend/src/components/Gamepage.js
@@ -50,8 +50,16 @@ function Gamepage() {
     // handle handles when a key is pressed
     function handleKeypress(event) {
       const lettersConst = [...letters];
+      // check cases for special key presses
+      if (event.code == "Enter") {
+        // PLACEHOLDER until a new function is added that checks if the word is valid 
+        handleEnter(curRow, curCol, "worde");
+      }
+      else if (event.code == "Backspace") {
+        handleBackspace(curRow, curCol);
+      }
       // validate key press to only allow letters
-      if ((event.code[3]).match(/[a-z]/i)) {
+      else if ((event.code[3]).match(/[a-z]/i)) {
         if (curCol === 5) {
           // letter not added if row already full
           return;
