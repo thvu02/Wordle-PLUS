52fbde914fd3c21341ede3619d5e211495ec187e
Nhamakami7 <nicholas_hamakami7@outlook.com>
2022-03-10 15:00:31 -0800

    Removed unnecessary imports in App and Index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';  
 import Homepage from './Homepage';
 import Gamepage from './components/Gamepage';
 import Leaderboard from './components/Leaderboard';
 import Infopage from './components/Infopage'
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import {useState} from 'react';
 import './App.css';
 
 function App() {
-    const [difficulty, setDifficulty] = useState('hard');
-    
     return (
         <BrowserRouter>
         <Routes>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';  
-import Homepage from './Homepage';
-import Gamepage from './components/Gamepage';
-import Leaderboard from './components/Leaderboard';
-import Infopage from './components/Infopage'
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import {useState} from 'react';
 import App from './App';
 
 ReactDOM.render(
