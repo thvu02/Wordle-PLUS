986714c4fd63983ba314e34795cf97f46c7fd41a
Trung Vu <trungvu08@g.ucla.edu>
2022-03-10 22:40:19 -0800

    Revert "testing purposes only; not good code"
    This reverts commit 922880098aa0afe56c2e68b1c75c1ee381cb7240.

--- frontend/src/components/Leaderboard.js
+++ frontend/src/components/Leaderboard.js
@@ -1,15 +1,12 @@
-import React from 'react'; 
-import { Component } from 'react';
+import React, { Component } from 'react';
 import './Leaderboard.css';
-import { BASE_URL } from ".././config.js";
-import Table from 'react-bootstrap/Table';
-import Axios from 'axios';
-
 import { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { useNavigate } from 'react-router-dom';
 
-import navHelper from './leaderboardComps/navHelper.js';
+import { BASE_URL } from ".././config.js";
+import Table from 'react-bootstrap/Table';
+import Axios from 'axios';
 
 const axios = Axios.create({
   baseURL: BASE_URL,
@@ -39,10 +36,10 @@ class Leaderboard extends Component {
 
   handleClick(e) {
     var temp = e.target.id;
-    if (temp === "fourLetters") {
+    if (temp == "fourLetters") {
       this.setState(state => ( {filterAssist: "Easy"}));
     }
-    else if (temp === "sixLetters") {
+    else if (temp == "sixLetters") {
       this.setState(state => ( {filterAssist: "Hard"}));
     }
     else {
@@ -52,51 +49,8 @@ class Leaderboard extends Component {
 
   render() {
     const { data, filterAssist } = this.state;
-
     return (
       <div>
-
-        <div class="nav-bar">
-          {/* <span id="l-nav-item">
-            <button variant="primary" id = "l-button" onClick={() => setShow(true)}> INSTRUCTION </button>
-            <Modal size ="lg" show={show} onHide={() => setShow(false)}>
-              <Modal.Header closeButton>
-                <Modal.Title>HOW TO PLAY</Modal.Title>
-              </Modal.Header>
-              <Modal.Body class="modal-txt">
-                <p id = "text1">
-                Guesses the WORDLE in six tries.
-                <br />
-                Each Guess must be a valid five-letter word. Hit the enter button to submit.
-                <br />
-                After each guess, the color of the tiles will change to show how close your guess was to the word.
-                </p> 
-                <hr id="line" />
-                <div id="display-flex">
-                  <button id="green"> E </button> 
-                  <p id="txt"> The letter E is in the word and in the correct spot. </p> <br />
-                </div>
-                <div id="display-flex">
-                  <button id="yellow" > G  </button>
-                  <p id="txt"> The letter G is in the word but in the wrong spot. </p> 
-                </div>
-                <div id="display-flex">
-                  <button id="gray" > G </button>
-                  <p id="txt"> The letter G is not in the word in any spot. </p> 
-                </div>
-                <hr id="line" />
-                <p> A new WORDLE will be available each day! </p> 
-              </Modal.Body>
-            </Modal>
-          </span>  */}
-          <span >
-            <p id ="c-nav-item">CS35L</p>
-          </span>   
-          <span id ="r-nav-item">
-            <button id = "r-button" onClick={() => navHelper('/')}> MAIN MENU </button>
-          </span>
-        </div>
-
         <div classname="threeButtons">
           <button id="fourLetters" onClick={this.handleClick}>Easy Mode Rankings</button>
           <button id="fiveLetters" onClick={this.handleClick}>Medium Mode Rankings</button>
@@ -114,7 +68,7 @@ class Leaderboard extends Component {
             </thead>
             <tbody>
               {
-                data.filter(player => (player.gameVersion === filterAssist)).map((row, index) => (
+                data.filter(player => (player.gameVersion == filterAssist)).map((row, index) => (
                   <tr>
                     <td>{"#" + (index + 1)}</td>
                     <td>{row.playerName}</td>
---
+++ frontend/src/components/leaderboardComps/samples.js
@@ -0,0 +1,22 @@
+export const Leaderboard = [
+    {
+        name: "Emily Gu",
+        score: 600,
+        time: 123
+    },
+    {
+        name: "Gene Block",
+        score: 100,
+        time: 1919
+    },
+    {
+        name: "David Smallberg",
+        score: 400,
+        time: 69
+    },
+    {
+        name: "Paul Eggert",
+        score: 500,
+        time: 303
+    }
+]
